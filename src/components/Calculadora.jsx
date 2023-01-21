79d2ee930ae6c11e2dea6d7c201ddea4346cec75
RonaldoFidelis <ronaldofidelis.ti@gmail.com>
2023-01-21 15:53:32 -0300

    feat:AdjustmentCss

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -6,43 +6,50 @@ import './Calculadora.css';
 const Calculadora = () => {
     const [num, setNum] = useState(0);
 
-    function handleClick (e){
+    function handleClick(e) {
         let input = e.target.value;
         if (num === 0) {
-            setNum (input)
-        }else{
+            setNum(input)
+        } else {
             setNum(num + input)
         }
-    } 
-    function clearDisplay (e){
+    }
+    function clearDisplay() {
         setNum(0)
-    } 
+    }
 
     return (
-        <div>
+        <div className="container">
             <Box m={5} />
+            <h1 className="title">Calculadora</h1>
             <Container maxWidth='xs'>
                 <div className="wrapper">
-                    <h1 className="resultado">{num}</h1>
-                    <button className="btnSuperior" onClick={clearDisplay}>AC</button>
-                    <button className="btnSuperior">+/-</button>
-                    <button className="btnSuperior">%</button>
-                    <button className="btnLateralDireita">/</button>
-                    <button className="btnNumPad" onClick={handleClick} value={7} >7</button>
-                    <button className="btnNumPad">8</button>
-                    <button className="btnNumPad">9</button>
-                    <button className="btnLateralDireita">X</button>
-                    <button className="btnNumPad">4</button>
-                    <button className="btnNumPad">5</button>
-                    <button className="btnNumPad">6</button>
-                    <button className="btnLateralDireita">-</button>
-                    <button className="btnNumPad">1</button>
-                    <button className="btnNumPad">2</button>
-                    <button className="btnNumPad">3</button>
-                    <button className="btnLateralDireita">+</button>
-                    <button className="btnNumPad btnZero">0</button>
-                    <button className="btnNumPad">,</button>
-                    <button className="btnLateralDireita"> = </button>
+                    <Box m={5} />
+                    <div className="resultado">{num}</div>
+                    <div className="btnsCalculadora">
+                        <button className="btnSuperior" onClick={clearDisplay}>AC</button>
+                        <button className="btnSuperior">+/-</button>
+                        <button className="btnSuperior">%</button>
+                        <button className="btnLateralDireita">/</button>
+                        <button className="btnNumPad" onClick={handleClick} value={7} >7</button>
+                        <button className="btnNumPad">8</button>
+                        <button className="btnNumPad">9</button>
+                        <button className="btnLateralDireita">X</button>
+                        <button className="btnNumPad">4</button>
+                        <button className="btnNumPad">5</button>
+                        <button className="btnNumPad">6</button>
+                        <button className="btnLateralDireita">-</button>
+                        <button className="btnNumPad">1</button>
+                        <button className="btnNumPad">2</button>
+                        <button className="btnNumPad">3</button>
+                        <button className="btnLateralDireita">+</button>
+                    </div>
+                    <div className="btnsUltimaFileira">
+                        <button className="btnNumPad btnZero">0</button>
+                        <button className="btnNumPad">,</button>
+                        <button className="btnLateralDireita"> = </button>
+                    </div>
+
                 </div>
             </Container>
         </div>
