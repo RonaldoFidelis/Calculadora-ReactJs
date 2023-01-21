68394335e05318fae22c4d9de5cf83ade3cfd2b5
RonaldoFidelis <ronaldofidelis.ti@gmail.com>
2023-01-21 17:56:07 -0300

    feat:finalize

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -5,19 +5,60 @@ import './Calculadora.css';
 
 const Calculadora = () => {
     const [num, setNum] = useState(0);
+    const [numAntigo, setNumAntigo] = useState(0);
+    const [operador, setOperador] = useState();
+    // let res = document.getElementsByClassName(".resultado");
 
     function handleClick(e) {
         let input = e.target.value;
+
         if (num === 0) {
             setNum(input)
         } else {
             setNum(num + input)
         }
     }
+
     function clearDisplay() {
         setNum(0)
     }
 
+    function porcetagem() {
+        setNum(num / 100)
+    }
+
+    function trocarOperador() {
+        if (num > 0) {
+            setNum(-num)
+        } else {
+            setNum(-num)
+        }
+    }
+
+    function operacao(e) {
+        let op = e.target.value;
+        setOperador(op);
+        setNumAntigo(num);
+        setNum(0)
+    }
+
+    function calculo() {
+        if (operador === "X") {
+            setNum(numAntigo * num );
+        }
+        if (operador === "/") {
+            setNum(numAntigo / num );
+        }
+        if (operador === "-") {
+            setNum(numAntigo - num );
+        }
+        if (operador === "+") {
+            let numOld = Number(numAntigo);
+            let newNum = Number(num);
+            let sum = numOld + newNum;
+            setNum(sum);
+        }
+    }
     return (
         <div className="container">
             <Box m={5} />
@@ -28,26 +69,26 @@ const Calculadora = () => {
                     <div className="resultado">{num}</div>
                     <div className="btnsCalculadora">
                         <button className="btnSuperior" onClick={clearDisplay}>AC</button>
-                        <button className="btnSuperior">+/-</button>
-                        <button className="btnSuperior">%</button>
-                        <button className="btnLateralDireita">/</button>
+                        <button className="btnSuperior" onClick={trocarOperador}>+/-</button>
+                        <button className="btnSuperior" onClick={porcetagem}>%</button>
+                        <button className="btnLateralDireita" onClick={operacao} value={'/'}>/</button>
                         <button className="btnNumPad" onClick={handleClick} value={7} >7</button>
-                        <button className="btnNumPad">8</button>
-                        <button className="btnNumPad">9</button>
-                        <button className="btnLateralDireita">X</button>
-                        <button className="btnNumPad">4</button>
-                        <button className="btnNumPad">5</button>
-                        <button className="btnNumPad">6</button>
-                        <button className="btnLateralDireita">-</button>
-                        <button className="btnNumPad">1</button>
-                        <button className="btnNumPad">2</button>
-                        <button className="btnNumPad">3</button>
-                        <button className="btnLateralDireita">+</button>
+                        <button className="btnNumPad" onClick={handleClick} value={8}>8</button>
+                        <button className="btnNumPad" onClick={handleClick} value={9}>9</button>
+                        <button className="btnLateralDireita" onClick={operacao} value={'X'}>X</button>
+                        <button className="btnNumPad" onClick={handleClick} value={4}>4</button>
+                        <button className="btnNumPad" onClick={handleClick} value={5}>5</button>
+                        <button className="btnNumPad" onClick={handleClick} value={6}>6</button>
+                        <button className="btnLateralDireita" onClick={operacao} value={'-'}>-</button>
+                        <button className="btnNumPad" onClick={handleClick} value={1}>1</button>
+                        <button className="btnNumPad" onClick={handleClick} value={2}>2</button>
+                        <button className="btnNumPad" onClick={handleClick} value={3}>3</button>
+                        <button className="btnLateralDireita" onClick={operacao} value={'+'}>+</button>
                     </div>
                     <div className="btnsUltimaFileira">
-                        <button className="btnNumPad btnZero">0</button>
-                        <button className="btnNumPad">,</button>
-                        <button className="btnLateralDireita"> = </button>
+                        <button className="btnNumPad btnZero" onClick={handleClick} value={0}>0</button>
+                        <button className="btnNumPad" onClick={handleClick} value={"."}>,</button>
+                        <button className="btnLateralDireita" onClick={calculo}> = </button>
                     </div>
 
                 </div>
