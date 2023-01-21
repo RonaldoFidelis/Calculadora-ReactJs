9df7d2f5044df15c0d62df6831f640d1fc9cd0e8
RonaldoFidelis <ronaldofidelis.ti@gmail.com>
2023-01-21 20:15:54 -0300

    feat:adjustmentCss

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -17,10 +17,13 @@ const Calculadora = () => {
         } else {
             setNum(num + input)
         }
+
     }
 
     function clearDisplay() {
-        setNum(0)
+        setNum(0);
+        setOperador();
+        setNumAntigo();
     }
 
     function porcetagem() {
@@ -37,20 +40,26 @@ const Calculadora = () => {
 
     function operacao(e) {
         let op = e.target.value;
-        setOperador(op);
-        setNumAntigo(num);
-        setNum(0)
+        if (num !== 0) {
+            setOperador(op);
+            setNumAntigo(num);
+            setNum(0);
+        }
+        if (setNumAntigo !== "") {
+            setOperador(op);
+        }
     }
 
     function calculo() {
-        if (operador === "X") {
-            setNum(numAntigo * num );
+
+        if (operador === "*") {
+            setNum(numAntigo * num);
         }
         if (operador === "/") {
-            setNum(numAntigo / num );
+            setNum(numAntigo / num);
         }
         if (operador === "-") {
-            setNum(numAntigo - num );
+            setNum(numAntigo - num);
         }
         if (operador === "+") {
             let numOld = Number(numAntigo);
@@ -58,15 +67,21 @@ const Calculadora = () => {
             let sum = numOld + newNum;
             setNum(sum);
         }
+        setOperador();
+        setNumAntigo();
     }
+
+
     return (
         <div className="container">
             <Box m={5} />
-            <h1 className="title">Calculadora</h1>
             <Container maxWidth='xs'>
                 <div className="wrapper">
                     <Box m={5} />
-                    <div className="resultado">{num}</div>
+                    <div className="resultado">
+                        <div className="previaCalculo">{numAntigo}{operador}</div>
+                        <div className="display">{num}</div>
+                    </div>
                     <div className="btnsCalculadora">
                         <button className="btnSuperior" onClick={clearDisplay}>AC</button>
                         <button className="btnSuperior" onClick={trocarOperador}>+/-</button>
@@ -75,7 +90,7 @@ const Calculadora = () => {
                         <button className="btnNumPad" onClick={handleClick} value={7} >7</button>
                         <button className="btnNumPad" onClick={handleClick} value={8}>8</button>
                         <button className="btnNumPad" onClick={handleClick} value={9}>9</button>
-                        <button className="btnLateralDireita" onClick={operacao} value={'X'}>X</button>
+                        <button className="btnLateralDireita" onClick={operacao} value={'*'}>X</button>
                         <button className="btnNumPad" onClick={handleClick} value={4}>4</button>
                         <button className="btnNumPad" onClick={handleClick} value={5}>5</button>
                         <button className="btnNumPad" onClick={handleClick} value={6}>6</button>
@@ -90,7 +105,6 @@ const Calculadora = () => {
                         <button className="btnNumPad" onClick={handleClick} value={"."}>,</button>
                         <button className="btnLateralDireita" onClick={calculo}> = </button>
                     </div>
-
                 </div>
             </Container>
         </div>
